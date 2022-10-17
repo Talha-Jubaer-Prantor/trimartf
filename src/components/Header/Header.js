c2ee4dd59a30afd6518f08aea665d9f888b19618
Talha-Jubaer-Prantor <talhajubaer3020@gmail.com>
2022-10-17 23:30:03 +0600

    fixing bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
         
             <nav className="navbar navbar-expand-lg bg-light nav-header">
   <div className="container-fluid">
-    <a className="navbar-brand logo" href="/"><img style={{"width":"200px"}} src="banerLogo.png" alt="" /> </a>
+    <a className="navbar-brand logo" href="/"><img style={{"width":"200px"}} src="banerLogo.PNG" alt="" /> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
